da37fb54403f0968c47cdcab9ce0271dfe66a4dc
Mauricio-HNS <90620341+Mauricio-HNS@users.noreply.github.com>
2025-03-07 11:49:23 -0300

    Commit-01
    Implantação Inicial do Projeto.

---
+++ flutter_power/bin/flutter_power.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_power/flutter_power.dart' as flutter_power;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flutter_power.calculate()}!');
+}
---
+++ flutter_power/test/flutter_power_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_power/flutter_power.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
